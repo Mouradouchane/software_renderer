00d78a8390a94814be8330e50f24245665a62b50
Mouradouchane <mouradouchane@gmail.com>
2024-01-14 23:48:21 +0100

    still suffring with tearing frames using SDL

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ uint32_t width  = 800;
 uint32_t height = 600;
 
 uint8_t random(uint8_t range_from, uint8_t range_to);
+uint32_t random32(uint32_t min, uint32_t max);
 
 int WINAPI WinMain(
 	HINSTANCE h_instance, HINSTANCE h_prev_instance, LPSTR p_cmd_line, int n_cmd_show
@@ -33,17 +34,18 @@ int WINAPI WinMain(
 		return 1;
 	}
 
+	// get window buffer
+	// NOTE : in resize you need to get a new surface !!!!!!!!!!
+	SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+
 	// create renderer 
-	SDL_Renderer* renderer = SDL_CreateRenderer( window , -1, SDL_RENDERER_ACCELERATED);
+	SDL_Renderer* renderer = SDL_CreateSoftwareRenderer( window_surface );
 
 	if (renderer == NULL) {
 		MessageBoxA(NULL, "SDL Failed to Create Renderer Because : " + *SDL_GetError() , 0, MB_ICONERROR);
 		return 1;
 	}
 
-	// get window buffer
-	// NOTE : in resize you need to get a new surface !!!!!!!!!!
-	SDL_Surface* window_surface = SDL_GetWindowSurface(window);
 
 	if (window_surface == NULL) {
 		MessageBoxA(NULL, "SDL Failed to Get Window Surface Because : " + *SDL_GetError(), 0, MB_ICONERROR);
@@ -53,15 +55,13 @@ int WINAPI WinMain(
 	bool quit = false;
 	SDL_Event sdl_event;
 
-	uint32_t* window_buffer   = (uint32_t*)(window_surface->pixels);
-	SDL_Surface* front_buffer = SDL_CreateRGBSurface(0 , width , height , 32 , 0, 0, 0, 0);
+	uint32_t* window_buffer = (uint32_t*)(window_surface->pixels);
 
-	/*
-	uint32_t* back_buffer   = new uint32_t[window_surface->w * window_surface->h];
-	uint32_t* swap_buffer   = front_buffer;
-	*/
+	uint32_t* front = new uint32_t[ width * height ];
+	uint32_t* back  = new uint32_t[width * height];
 
-	// main loop 
+
+	// core loop 
 	while ( !quit ) {
 		
 		while ( SDL_PollEvent(&sdl_event) ) {
@@ -91,26 +91,31 @@ int WINAPI WinMain(
 		// clear old frame
 		SDL_RenderClear(renderer);
 
-		// SDL_LockSurface(window_surface);
-
-		uint32_t* buffer = (uint32_t *)front_buffer->pixels;
+		// lock surface buffer
+		SDL_LockSurface(window_surface);
 
 		// process new frame
 		for (size_t y = 0; y < height; y += 1) {
+			size_t  Y = (y * width);
 			for (size_t x = 0; x < width; x += 1) {
-				buffer[x + (y * width)] = SDL_MapRGB(window_surface->format, x*random(0, 255), y*random(0, 255) , random(0, 255));
+				front[Y + x] = SDL_MapRGB(window_surface->format, x * random(0, 255), x * y * random(0, 255), random(0, 255));
+				back[Y + x] = SDL_MapRGB(window_surface->format, x * random(0, 255), x * y * random(0, 255), random(0, 255));
 			}
 		}
-		// SDL_UnlockSurface(window_surface);
 
-		SDL_BlitSurface(front_buffer, 0, window_surface, 0);
-		if (SDL_UpdateWindowSurface(window) != 0 ) {
-			MessageBoxA(NULL, "SDL Failed to Render Buffer Into Window Because : " + *SDL_GetError(), 0, MB_ICONERROR);
-			quit = true;
-		}
-		
-	
+		CopyMemory( window_buffer, back, (width * height * sizeof(uint32_t)) );
+
+		// unlock surface buffer
+		SDL_UnlockSurface(window_surface);
+
+		SDL_UpdateWindowSurface(window);
+		window_buffer = (uint32_t*)(window_surface->pixels);
+		std::swap(front, back);
+
 	}
+
+	delete[] front;
+	delete[] back;
 
 	// free sdl stuff
 	SDL_DestroyRenderer(renderer);
@@ -124,13 +129,15 @@ int WINAPI WinMain(
 // end : main function
 
 uint8_t random(uint8_t min , uint8_t max) {
-	/*
-	std::random_device                  rand_dev;
-	std::mt19937                        generator(rand_dev());
-	std::uniform_int_distribution<int>   distr(range_from, range_to);
-	return (uint8_t)distr(generator);
-	*/
 	std::srand(time(0));
+	
 	return (uint8_t)(std::rand() - max + min);
+
+}
+
+uint32_t random32(uint32_t min, uint32_t max) {
+	std::srand(time(0));
+
+	return (uint32_t)(std::rand() - max + min);
 
 }
